e60a2b0fa8518322966fce7298e5c4de7d088d7b
ssar@nate.com <ssar@nate.com>
2021-12-26 21:24:00 +0900

    80포트로 변경

--- src/main/java/com/cos/blogactionaws/web/BlogController.java
+++ src/main/java/com/cos/blogactionaws/web/BlogController.java
@@ -1,5 +1,7 @@
 package com.cos.blogactionaws.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BlogController {
 
 	@GetMapping("/")
-	public String home() {
-		return "<h1>Home</h1>";
+	public ResponseEntity<String> home() {
+		return new ResponseEntity<>("<h1>Home</h1>", HttpStatus.OK);
 	}
 }
